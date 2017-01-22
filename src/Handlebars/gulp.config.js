080c5acb5ec4b4dbd834b611b1dd2ae1aa27da72
trodham <tim.rodham@midcounties.coop>
2017-01-22 09:58:53 +0000

    Initial commit

---
+++ src/Handlebars/gulp.config.js
@@ -0,0 +1,16 @@
+﻿module.exports = function () {
+    var root = './';
+    var assetRoot = root + 'wwwroot/';
+    var handlebarsRoot = assetRoot + 'templates/';
+
+    var config = {
+        templatePath: handlebarsRoot,
+        templatePartialPath: handlebarsRoot + 'partials',
+        templateOutputPath: handlebarsRoot + 'dist',
+        templates: [
+            handlebarsRoot + '**/*.hbs'
+        ]
+    };
+
+    return config;
+};
---
+++ src/Handlebars/gulpfile.js
@@ -0,0 +1,30 @@
+﻿/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp');
+var handlebars = require('handlebars');
+var config = require('./gulp.config')();
+var gulpHandlebars = require('gulp-handlebars-html')(handlebars);
+var regexRename = require('gulp-regex-rename');
+var replace = require('gulp-replace');
+
+gulp.task('compileHtml', function () {
+    var templateData = {
+
+    },
+    options = {
+        partialsDirectory: [config.templatePartialPath]
+    };
+
+    return gulp.src(config.templatePath + "*.page.hbs")
+               .pipe(gulpHandlebars(templateData, options))
+               .pipe(regexRename(/\.page\.hbs$/, ".html"))
+               .pipe(replace(/\uFEFF/ig, "")) //cut out zero width nbsp characters the compiler adds in
+               .pipe(gulp.dest(config.templateOutputPath));
+});
+
+gulp.task('compileHtml:watch', function () {
+    return gulp.watch(config.templates, ['compileHtml']);
+});
